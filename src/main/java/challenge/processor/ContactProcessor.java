7c9963c2be65cbda75b823eec2cf1ba957f517a9
matballesteros <matballesteros@paypal.com>
2024-08-13 14:27:38 -0300

    optimize algorithm

--- src/main/java/challenge/processor/ContactProcessor.java
+++ src/main/java/challenge/processor/ContactProcessor.java
@@ -3,7 +3,9 @@ package challenge.processor;
 import challenge.dto.Contact;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static challenge.comparator.ContactComparator.compare;
 
@@ -11,16 +13,31 @@ public class ContactProcessor {
 
     public static List<String> identifyPossibleDuplicates(List<Contact> contacts) {
         List<String> results = new ArrayList<>();
+        Set<String> processedPairs = new HashSet<>();
 
         for (int i = 0; i < contacts.size(); i++) {
+            Contact contact1 = contacts.get(i);
             for (int j = i + 1; j < contacts.size(); j++) {
-                String accuracy = compare(contacts.get(i), contacts.get(j));
-                if (!accuracy.equals("None")) {
-                    results.add("Contact ID: " + contacts.get(i).getContactID() + " matches Contact ID: " + contacts.get(j).getContactID() + " with Accuracy: " + accuracy);
+                Contact contact2 = contacts.get(j);
+                String pairKey = generatePairKey(contact1.getContactID(), contact2.getContactID());
+                if (!processedPairs.contains(pairKey)) {
+                    String accuracy = compare(contact1, contact2);
+                    if (!accuracy.equals("None")) {
+                        String result = "Contact ID: " + contact1.getContactID() +
+                                " matches Contact ID: " + contact2.getContactID() +
+                                " with Accuracy: " + accuracy;
+                        results.add(result);
+                    }
+                    processedPairs.add(pairKey);
                 }
             }
         }
+
         return results;
+    }
+
+    private static String generatePairKey(int id1, int id2) {
+        return Math.min(id1, id2) + "-" + Math.max(id1, id2);
     }
 }
 
